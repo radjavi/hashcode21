b002a59be978b3ac851efdd458952e56858df0c6
radjavi <radjavi@hotmail.com>
2021-02-25 20:31:34 +0100

    bad solution

--- solve.py
+++ solve.py
@@ -1,3 +1,5 @@
+from sortedcollections import ValueSortedDict
+
 # Solver that takes an input as a file object,
 # and returns the output as a string
 def solve(inp):
@@ -12,6 +14,8 @@ def solve1(inp):
     F = line1[4]
 
     s = 0
+    streets = {}
+    intersections = {}
     # Street descriptions
     while s < S:
         line = inp.readline().strip().split(' ')
@@ -19,27 +23,72 @@ def solve1(inp):
         E = int(line[1])
         street_name = line[2]
         L = int(line[3])
+        streets[street_name] = {
+            "start": B,
+            "end": E,
+            "length": L,
+        }
+        if B not in intersections:
+            intersections[B] = {
+                "green_lights": [],
+                "incoming": [],
+                "outgoing": [street_name],
+            }
+        else:
+            intersections[B]["outgoing"].append(street_name)
+        if E not in intersections:
+            intersections[E] = {
+                "green_lights": [],
+                "incoming": [street_name],
+                "outgoing": [],
+            }
+        else:
+            intersections[E]["incoming"].append(street_name)
         s += 1
     
     v = 0
+    starting_street_count = ValueSortedDict()
+    cars = []
     # Paths of each car
     while v < V:
         line = inp.readline().strip().split(' ')
         P = int(line[0])
+        car = []
         for p in range(P):
-            street = line[p]
+           car.append(line[p])
+        cars.append(car)
+        if line[1] not in starting_street_count:
+            starting_street_count[line[1]] = 1
+        else:
+            starting_street_count[line[1]] += 1
         v += 1
 
-    intersections = {}
+    print(starting_street_count)
+    
+    for starting_street in starting_street_count.__reversed__():
+        E = streets[starting_street]["end"]
+        duration = starting_street_count[starting_street]
+        if len(intersections[E]["green_lights"]) == 0:
+            intersections[E]["green_lights"].append({
+                "name": starting_street,
+                "duration": D,
+            })
+
+    for intersection in intersections:
+        if not intersections[intersection]["green_lights"]:
+            intersections[intersection]["green_lights"].append({
+                "name": intersections[intersection]["incoming"][0],
+                "duration": D,
+            })
 
     out_str = f'{len(intersections)}\n'
     for intersection in intersections:
-        out_str += f'{intersection.id}\n'
-        out_str += f'{intersection.nr_incoming}\n'
-        for green_light in intersection.green_lights:
-            out_str += f'{green_light.name}'
+        out_str += f'{intersection}\n'
+        out_str += f'{len(intersections[intersection]["green_lights"])}\n'
+        for green_light in intersections[intersection]["green_lights"]:
+            out_str += f'{green_light["name"]}'
             out_str += ' '
-            out_str += f'{green_light.duration}'
+            out_str += f'{green_light["duration"]}'
             out_str += '\n'
 
-    return ""
+    return out_str
